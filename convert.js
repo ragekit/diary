c71d7027ebb2119d022f5328f31c857238b6fcdb
RageKit <bgattet@gmail.com>
2015-03-04 16:36:51 +0100

    new version with nedb

--- convert.js
+++ convert.js
@@ -1,9 +1,10 @@
 var yaml = require('yamljs');
 var yamlPath = "C:/Users/Benjamin/Documents/Dropbox/datadiary.yaml";
-var mongoose = require('mongoose');
 var data  = yaml.load(yamlPath);
+var Datastore = require('nedb')
+  , db = new Datastore({ filename: './db', autoload: true })
 
-
+var moment = require("moment");
 
 var projects = [];
 for (var i = 0; i < data.length; i++) {
@@ -19,120 +20,45 @@ for (var i = 0; i < data.length; i++) {
 				{
 					projects[key2.toString().toLowerCase()] = [];
 				}
-				var key = key.toString().split("/");
-				key.reverse();
-				key = key.join("-");
+			
 				var l = log[key2];
-
-				l.start = l.start.split("h");
-				l.start = l.start.join(":");
-
-				l.start += "+01:00";
-
-				l.end = l.end.split("h");
-				l.end = l.end.join(":");
-				l.end += "+01:00";
-
-				l.start = new Date(key +"T"+ l.start);
-				l.end = new Date(key +"T"+ l.end);
+				l.start = l.start.split("h").join(" ");
+				l.end = l.end.split("h").join(" ");
+				console.log(key + " " + l.start);
+				console.log(key + " " + l.end);
+				l.start = moment(key + " " + l.start, "DD/MM/YYYY HH mm ");
+				l.end  = moment(key + " " + l.end, "DD/MM/YYYY HH mm ");
+				console.log(l.start.format("D MMMM YYYY, H:mm:ss"));
+				console.log(l.end.format("D MMMM YYYY, H:mm:ss"));
 				projects[key2.toString().toLowerCase()].push(l);
 			}
 		}
 	}	
 }
-//console.log(new Date(Date.now()));
 
-var toRemove = [];
+var projectSchema = function(){
+	this.name = "";
+	this.logs = [];
+	this._started = false;
+}
+
+var dbproject = []
 
 for(var key in projects)
 {
-	var p = projects[key];
-	for(var log in p)
-	{
-		var l = p[log];
-		if(l.end == "Invalid Date")
-		{
-
-			var projectLogs = p;
-
-			var corrected = {start:0,end:0};
-
-			var nextDay = new Date(l.start);
-			nextDay.setDate(nextDay.getDate()+1);
-			//console.log(nextDay);
-
-			for(var plog in projectLogs)
-			{
-				ll = projectLogs[plog];
-				//console.log(ll.start.getHours() +" // "+ nextDay.getDate())
-
-				if(ll.start.getHours() == 0)
-				{
-					var sdate = new Date(ll.start).setHours(0, 0, 0, 0);
-					var edate = new Date(nextDay).setHours(0, 0, 0, 0);
-					//console.log(sdate);
-					if(sdate == edate)
-					{
-						toRemove.push(ll);
-						toRemove.push(l);
-
-						corrected.start = l.start;
-						corrected.end = ll.end;
-						p.push(corrected);
-						//console.log(corrected);
-					}
-				}
-				
-			}
-
-			corrected.start = l.start;
-
-		}
-		//console.log(l);
-	}
-}
-
-for (var i = 0; i < toRemove.length; i++) {
-	for(var key in projects)
-	{
-		project = projects[key];
-		if(project.indexOf(toRemove[i])>-1)
-		{
-			projects.splice(project.indexOf(toRemove[i]));
-		}
-	}
-}
-
-
-var projectSchema = new mongoose.Schema({
-	name: String,
-	logs: [{
-		start: Date,
-		end: Date
-	}]
-})
-
-var dbpath = "db";
-
-var Project = mongoose.model('Project', projectSchema);
-
-
-var mongooseProjects = []
-
-for(var key in projects)
-{
-	var p = new Project({name:key.toString()})
-	mongooseProjects.push(p);
+	var p = new projectSchema();
+	p.name = key.toString();
+	dbproject.push(p);
 	var project = projects[key];
-	for(var log in project)
-	{
-		//console.log(project[log]);
-		p.logs.push(project[log]);
+	project = project.sort(function(a,b){
+		return a.start.valueOf() - b.start.valueOf();
+	})
+	for (var i = 0; i < project.length; i++) {
+		p.logs.push(project[i]);
 	}
 }
 
-mongoose.connect('mongodb://localhost/test');
 
-for (var i = 0; i < mongooseProjects.length; i++) {
-	mongooseProjects[i].save();
+for (var i = 0; i < dbproject.length; i++) {
+	db.insert(dbproject[i]);
 };
--- diary.js
+++ diary.js
@@ -1,142 +1,134 @@
-#!/usr/bin/env node
+var Datastore = require('nedb')
+  , db = new Datastore({ filename: './db', autoload: true })
+  , moment = require('moment');
 
-var mongoose = require('mongoose');
-var spawn = require('child_process').spawn;
-    
-var projectSchema = new mongoose.Schema({
-	name: String,
-	logs: [{
-		start: Date,
-		end: Date
-	}]
-})
-
-var dbpath = "db";
-
-var Project = mongoose.model('Project', projectSchema);
-
+var projectSchema = function(){
+	this.name = "";
+	this.logs = [];
+	this._started = false;
+}
 var Diary = {};
 
 Diary.add = function(projectName,cb)
 {
 
-	Project.find({name:projectName},function(err,found){
+	db.find({name:projectName},function(err,found){
 		if(found.length ==0)
 		{
-			var p = new Project({name:projectName});
-			p.save(function(err)
+			var p = new projectSchema();
+			p.name = projectName
+			db.insert(p,function(err)
 			{
 				if(err) return console.error(err);
 				else{
 					console.log(projectName + " added");
 				}
-				Diary.close();
 			})
 		}else
 		{
 			console.error(projectName + " allready exist");
-			Diary.close();
 		}
 
 	})
 }
 
-Diary.start = function(projectName,cb)
+Diary.start = function(projectName)
 {
-	Project.findOne({name:projectName},function(err,found){
+	db.findOne({name:projectName},function(err,found){
 		if(found == null)
 		{
 			console.log("no project named" + projectName + " found");
-			Diary.close();
+			return
 		}else
 		{
-			Project.findOne().where('logs').elemMatch({end:null}).exec(function(err,started){
-				console.log(started);
-				if(started != null)
-				{
-					if(started.name != found.name)
-					{
-						console.log("project " + started.name + " allready started, ending");
-						Diary.end(function(){
-							startlog(started,cb)
-						})
-					}else
-					{
-						console.log("project allready started");
-						Diary.close();
+			if(!found._started)
+			{
+				db.update({_id : found._id},{
+					$addToSet:{
+						logs:{start: new Date(),end:null}
+					},
+					$set:{
+						_started:true
 					}
-				}else
-				{
-					startlog(found,cb);
-				}
-			})		
+				})
+			}else
+			{
+				console.log("project allready started !");
+			}	
 		}
 	})
 }
 
-Diary.list = function(cb){
-	Project.find({},function(err,found){
-		for (var i = 0; i < found.length; i++) {
-			console.log(found[i].name);
-		};
-		Diary.close();
-	})
-}
+Diary.end = function(projectName)
+{
+	if(projectName == undefined)
+	{
+		db.find({ $where: function () { 
+			for (var i = 0; i < this.logs.length; i++) {
+				var item = this.logs[i];
+				if(item.end == null) return true
+			};
+			return false;
+		} }, function (err, docs) {
+		  for (var i = 0; i < docs.length; i++) {
+		  	docs[i]._started = false;
+		  	docs[i].logs[docs[i].logs.length-1].end = new Date();
 
-startlog = function(project,cb){
-	var l = {start:Date.now(),end:null};
-	project.logs.push(l);
-	console.log("starting project "  + project.name);
-	project.save(function(){
-		Diary.close();
-	});
-}
+		  	db.update({_id: docs[i]._id},docs[i]);
+		  };
+		});
 
-Diary.end = function(cb){
-	Project.findOne().where('logs').elemMatch({end:null}).exec(function(err,found){
+		console.log("all project ended");
+		return;
+	}
+
+	db.findOne({name:projectName},function(err,found){
 		if(found == null)
 		{
-			console.log("no project have been started");
-			Diary.close();
-			return;
+			console.log("no project named" + projectName + " found");
+			return
+		}else
+		{
+			if(found._started)
+			{
+				found.logs[found.logs.length-1].end = new Date();
+				found._started = false;
+				db.update({_id : found._id},found);
+			}else
+			{
+				console.log("project not started !");
+			}
 		}
-		found.logs[found.logs.length-1].end = Date.now();
-		found.save(function(){
-			Diary.close();
-		});
 	})
 }
 
-Diary.close = function(){
-	mongoose.connection.close();	
+Diary.list = function(){
+	db.find({},function(err,found){
+		for (var i = 0; i < found.length; i++) {
+			console.log();
+			console.log(found[i].name);
+			console.log("  │");
+			var totaldiff = 0;
+			for (var j = 0; j < found[i].logs.length; j++) {
+				var s = moment(found[i].logs[j].start);
+				var e = moment(found[i].logs[j].end);
+				var diff = e.diff(s);
+				totaldiff += diff;
+				console.log("  ├───" + s.format("D MMMM YYYY, H:mm:ss") + " // " +e.format("D MMMM YYYY, H:mm:ss") +" == " + moment.duration(diff).humanize());
+			};
+			console.log("  ╚══  total time : " + moment.duration(totaldiff).humanize());
+
+		};
+	})
 }
 
-
-var mongo = spawn('mongod', ['--dbpath', 'db']);
-
-mongo.stdout.on('data', function (data) {
-  if(data.toString().indexOf("waiting for connections") >0)
-  {
-  	console.log("mongo started");
-  	mongoose.connect('mongodb://localhost/test');
-    var args = process.argv.slice(2);
+var args = process.argv.slice(2);
 	arguments = args.slice(1);
-	if(diary[args[0]] != null)
+	if(Diary[args[0]] != null)
 	{
 		Diary[args[0]].apply(this,arguments);
 	}
 	else
 	{
 		console.log(args[0] + " param doesn't exist");
-		Diary.close();
 	}
-  }
-});
-
-mongo.stderr.on('data', function (data) {
-  
-});
-
-mongo.on('close', function (code) {
-  console.log('child process exited with code ' + code);
-});
